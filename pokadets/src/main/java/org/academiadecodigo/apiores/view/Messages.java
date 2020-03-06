b1badf43e4b9126ce42c585923bea49d84bf2f20
sara <emailsdasara@gmail.com>
2020-03-06 17:29:27 -0100

    more messages configurated

--- pokadets/src/main/java/org/academiadecodigo/apiores/view/Messages.java
+++ pokadets/src/main/java/org/academiadecodigo/apiores/view/Messages.java
@@ -3,15 +3,18 @@ package org.academiadecodigo.apiores.view;
 import org.academiadecodigo.apiores.models.Ability;
 import org.academiadecodigo.apiores.models.cadets.Pokadet;
 
+import java.security.PublicKey;
 import java.util.Map;
 
 public class Messages {
+
+    private static String winnerAscci;
 
     public final static String ABILITY_TO_USE = "\nWhat ability do you want to use?";
 
     public final static String INVALID_ABILITY = "";
 
-    public final static String  WELCOME_MESSAGE ="\n\n                                  WELCOME\n" +
+    public final static String  WELCOME ="\n\n                                  WELCOME\n" +
             "██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗\n" +
             "██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝\n" +
             "██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  \n" +
@@ -19,6 +22,39 @@ public class Messages {
             "╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗\n" +
             " ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝\n\n" +
             "Select your player:\n";
+
+    public final static String ABILITY_SELECTION = " select your move:"; // Vai ter nome do Pokadet antes desta String
+
+    public final static String WINNER = " you                       ### \n" +
+            " #    #  ####  #    # ### \n" +
+            " #    # #    # ##   # ### \n" +
+            " #    # #    # # #  #  #  \n" +
+            " # ## # #    # #  # #     \n" +
+            " ##  ## #    # #   ## ### \n" +
+            " #    #  ####  #    # ### \n" +
+            "\n Restart?";
+
+    
+    public final static String WINNER2 = winnerAscci + ("\n" +
+            "       ██╗    ██╗ ██████╗ ███╗   ██╗██╗\n" +
+            "       ██║    ██║██╔═══██╗████╗  ██║██║\n" +
+            "       ██║ █╗ ██║██║   ██║██╔██╗ ██║██║\n" +
+            "       ██║███╗██║██║   ██║██║╚██╗██║╚═╝\n" +
+            "       ╚███╔███╔╝╚██████╔╝██║ ╚████║██╗\n" +
+            "  You  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝    Restart?\n" +
+            "                                ");
+    
+    public final static String LOOSER = ("\n" +
+            "       ██╗      ██████╗ ███████╗████████╗      \n" +
+            "       ██║     ██╔═══██╗██╔════╝╚══██╔══╝      \n" +
+            "       ██║     ██║   ██║███████╗   ██║         \n" +
+            "       ██║     ██║   ██║╚════██║   ██║         \n" +
+            "       ███████╗╚██████╔╝███████║   ██║██╗██╗██╗\n" +
+            " You   ╚══════╝ ╚═════╝ ╚══════╝   ╚═╝╚═╝╚═╝╚═╝   Restart?\n" +
+            "                                        ");
+
+    //ascii generator: https://manytools.org/hacker-tools/ascii-banner/
+    //style: banner, font: ANSI Shadow
 
 
     public static String[]abilitiesMenu(Pokadet pokadet){
@@ -47,4 +83,15 @@ public class Messages {
 
         return pokadetsString;
     }
+
+    public static String[] restartMenu(){
+
+        return new String[]{"Yes", "No"};
+    }
+
+    public static void setWinnerAscci(String string){
+        winnerAscci = string;
+    }
+
+
 }
