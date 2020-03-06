bf4a31c45cb714ebf2795946e36509a257078cc2
Ruben Rocha <rubenbarbeiro@hotmail.com>
2020-03-05 23:25:35 -0100

    created controller class, going to merge with service class

---
+++ pokadets/src/main/java/org/academiadecodigo/apiores/controllers/PokadetController.java
@@ -0,0 +1,27 @@
+package org.academiadecodigo.apiores.controllers;
+
+import org.academiadecodigo.apiores.models.Pokadet;
+
+public class PokadetController {
+
+    private Pokadet loser;
+    private PokadetService service;
+    public void init(){
+
+
+        while(true){
+
+        }
+    }
+
+
+    public void setLoser(Pokadet loser) {
+        this.loser = loser;
+    }
+
+
+    public void setService(PokadetService service) {
+        this.service = service;
+    }
+
+}
