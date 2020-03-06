fa46a89aa651c94d5b5cad793b6a376428e853e4
sara <emailsdasara@gmail.com>
2020-03-06 16:59:10 -0100

    message Class: welcome + string methods

--- pokadets/src/main/java/org/academiadecodigo/apiores/view/Messages.java
+++ pokadets/src/main/java/org/academiadecodigo/apiores/view/Messages.java
@@ -11,6 +11,15 @@ public class Messages {
 
     public final static String INVALID_ABILITY = "";
 
+    public final static String  WELCOME_MESSAGE ="\n\n                                  WELCOME\n" +
+            "██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗\n" +
+            "██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝\n" +
+            "██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  \n" +
+            "██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  \n" +
+            "╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗\n" +
+            " ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝\n\n" +
+            "Select your player:\n";
+
 
     public static String[]abilitiesMenu(Pokadet pokadet){
 
@@ -19,12 +28,23 @@ public class Messages {
         String[] abilitiesString = new String[abilities.size()];
 
         for (int i = 0; i<abilities.size(); i++){
-            abilitiesString[i]=abilities.get(i).getName();
+            abilitiesString[i]=abilities.get(i+1).getName();
         }
         return abilitiesString;
     }
 
     public static String pokadetInfo(Pokadet pokadet){
         return "Hp: "+pokadet.getHp();
+    }
+
+    public String[] pokadetMenu (Map<Integer,Pokadet> map){
+
+        String[] pokadetsString = new String[map.size()];
+
+        for (int i = 0; i < map.keySet().size(); i++) {
+            pokadetsString[i] = map.get(i+1).getName();
+        }
+
+        return pokadetsString;
     }
 }
