4fe097a78a395370d9f029119815bb0800b7f57a
fabiomartins <fabio.stmartins.09@gmail.com>
2020-03-07 16:48:14 -0100

    Created a setAbilities method in the bootstrap class and applied it to clean up the code

--- pokadets/src/main/java/org/academiadecodigo/apiores/Bootstrap.java
+++ pokadets/src/main/java/org/academiadecodigo/apiores/Bootstrap.java
@@ -31,130 +31,100 @@ public class Bootstrap {
 
         Pokadet andre = new Andre();
         andre.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.ANDRE_ABILITY);
-        andre.setHp(107);
-        andre.setAttack(7);
-        andre.setDefense(5);
+        setStats(andre,107,7, 5);
         pokadetMap.put(1,andre);
 
         Pokadet bernardo = new Bernardo();
         bernardo.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.BERNARDO_ABILITY);
-        bernardo.setHp(105);
-        bernardo.setAttack(8);
-        bernardo.setDefense(5);
+        setStats(bernardo,105, 8, 5);
         pokadetMap.put(2,bernardo);
 
         Pokadet bruno = new Bruno();
         bruno.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.BRUNO_ABILITY);
-        bruno.setHp(110);
-        bruno.setAttack(5);
-        bruno.setDefense(8);
+        setStats(bruno, 110,5,8);
         pokadetMap.put(3,bruno);
 
         Pokadet claudia = new Claudia();
         claudia.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.CLAUDIA_ABILITY);
-        claudia.setHp(120);
-        claudia.setAttack(5);
-        claudia.setDefense(8);
+        setStats(claudia,120,5,8);
         pokadetMap.put(4,claudia);
 
         Pokadet dutra = new Dutra();
         dutra.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.DUTRA_ABILITY);
-        dutra.setHp(105);
-        dutra.setAttack(8);
-        dutra.setDefense(5);
+        setStats(dutra, 105, 8,5);
         pokadetMap.put(5,dutra);
 
         Pokadet evandro = new Evandro();
         evandro.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.EVANDRO_ABILITY);
-        evandro.setHp(120);
-        evandro.setAttack(4);
-        evandro.setDefense(8);
+        setStats(evandro, 120, 4, 8);
         pokadetMap.put(6,evandro);
 
         Pokadet fabio = new Fabio();
         fabio.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.FABIO_ABILITY);
-        fabio.setHp(120);
-        fabio.setAttack(7);
-        fabio.setDefense(9);
+        setStats(fabio, 120, 7, 9);
         pokadetMap.put(7,fabio);
 
         Pokadet francisco = new Francisco();
         francisco.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.FRANCISCO_ABILITY);
-        francisco.setHp(110);
-        francisco.setAttack(7);
-        francisco.setDefense(7);
+        setStats(francisco, 110, 7, 7);
         pokadetMap.put(8,francisco);
 
         Pokadet gustavo = new Gustavo();
         gustavo.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.GUSTAVO_ABILITY);
-        gustavo.setHp(90);
-        gustavo.setAttack(8);
-        gustavo.setDefense(6);
+        setStats(gustavo, 90,8,6);
         pokadetMap.put(9,gustavo);
 
         Pokadet hugo = new Hugo();
         hugo.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.HUGO_ABILITY);
-        hugo.setHp(110);
-        hugo.setAttack(7);
-        hugo.setDefense(8);
+        setStats(hugo,110,7,8);
         pokadetMap.put(10,hugo);
 
         Pokadet jaime = new Jaime();
         jaime.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.JAIME_ABILITY);
-        jaime.setHp(100);
-        jaime.setAttack(8);
-        jaime.setDefense(5);
+        setStats(jaime,100,8,5);
         pokadetMap.put(11,jaime);
 
         Pokadet julio = new Julio();
         julio.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.JULIO_ABILITY);
-        julio.setHp(115);
-        julio.setAttack(7);
-        julio.setDefense(7);
+        setStats(julio,115,7,7);
         pokadetMap.put(12,julio);
 
         Pokadet miguel = new Miguel();
         miguel.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.MIGUEL_ABILITY);
-        miguel.setHp(120);
-        miguel.setAttack(5);
-        miguel.setDefense(9);
+        setStats(miguel,120,5,9);
         pokadetMap.put(13,miguel);
 
         Pokadet paulo = new Paulo();
         paulo.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR, Ability.PAULO_ABILITY);
-        paulo.setHp(110);
-        paulo.setAttack(7);
-        paulo.setDefense(8);
+        setStats(paulo,110,7,8);
         pokadetMap.put(14,paulo);
 
         Pokadet pedro = new Pedro();
         pedro.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR,Ability.PEDRO_ABILITY);
-        pedro.setHp(115);
-        pedro.setAttack(6);
-        pedro.setDefense(9);
+        setStats(pedro,115,6,9);
         pokadetMap.put(15,pedro);
 
         Pokadet ruben = new Ruben();
         ruben.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR,Ability.RUBEN_ABILITY);
-        ruben.setHp(100);
-        ruben.setAttack(8);
-        ruben.setDefense(6);
+        setStats(ruben,100,8,6);
         pokadetMap.put(16,ruben);
 
         Pokadet sara = new Sara();
         sara.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR,Ability.SARA_ABILITY);
-        sara.setHp(100);
-        sara.setAttack(6);
-        sara.setDefense(8);
+        setStats(sara,100,6,8);
         pokadetMap.put(17,sara);
 
         Pokadet xavier = new Xavier();
         xavier.setAbilities(Ability.HEADBUTT,Ability.UPPERCUT,Ability.THROW_CHAIR,Ability.XAVIER_ABILITY);
-        xavier.setHp(110);
-        xavier.setAttack(7);
-        xavier.setDefense(7);
+        setStats(xavier, 110,7,7);
         pokadetMap.put(18,xavier);
 
         return server;
+    }
+
+    void setStats(Pokadet pokadet, int hp, int attack, int defense){
+        pokadet.setHp(hp);
+        pokadet.setAttack(attack);
+        pokadet.setDefense(defense);
     }
 }
